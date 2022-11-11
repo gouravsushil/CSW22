a28159923601f3b8d2f76d4f9511e5b38c0330e9
Sushil Gourav <97386225+gouravsushil@users.noreply.github.com>
2022-11-11 12:54:53 +0530

    tower of hanoi 1st atte,=mpt

---
+++ index.js
@@ -0,0 +1,23 @@
+/* $(".bt").click(function(){
+   $(".x").css({left:1000;});
+
+}); */
+
+      var str1 = $("#from").val();
+      var str2 = $("#to").val();
+
+$(document).ready(function(){
+    $(".bt").click(function(){
+      var position = $(".x").position();
+      //alert(position.left+" "+position.top);
+      //alert(str1+" "+str2); 
+
+
+     
+   
+      
+  });
+  
+});
+
+
